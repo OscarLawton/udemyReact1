d0aa33a87cbaf43705feb5355867365e9773a185
Emmet <emmetlawton@gmail.com>
2019-12-27 01:04:27 +0000

    react compoents and importing it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <h2>Hi, I'm a React Element</h2>
-      </div>
-    );
+        <div className="App">
+          <h2>Hi, I'm a React Element</h2>
+          <Person />
+        </div> 
+        
+      ); 
+    //return React.createElement('div', {className: "App"}, React.createElement('h1', null, "I'm learning React"))
   }
 }
 
