71d2f5402388f43aca759d54686162b29e2148f9
Emmet <emmetlawton@gmail.com>
2020-01-08 06:10:03 +0000

    manipulating the state

--- src/App.js
+++ src/App.js
@@ -11,11 +11,22 @@ class App extends Component {
     ]
   } 
 
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    // DONT DO THIS: this.state.persons[0].name = "Emmet Alfred"
+    this.setState({ persons: [
+                    {name: 'Maximus Desimus Meridimus', age: 28},
+                    {name: 'Manu', age: 29},
+                  {name: "Stephanie", age: 27}
+    ]})
+  }
+
   render() {
 
     return (
         <div className="App">
           <h2>Hi, I'm a React Element</h2>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Height: 6ft</Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
